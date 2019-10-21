23f875aacb9f664e5ea4b99fc32a8b8933d99cf1
drug007 <drug2004@bk.ru>
2019-10-21 17:05:33 +0300

    ProcessableMembers added

--- source/meta.d
+++ source/meta.d
@@ -14,13 +14,38 @@ private template isProperty(alias aggregate, string member)
 		enum bool isProperty = false;
 }
 
+private bool privateOrPackage()(string protection)
+{
+	return protection == "private" || protection == "package";
+}
+
+// check if the member is readable/writeble?
+private enum isReadableAndWritable(alias aggregate, string member) = __traits(compiles, __traits(getMember, aggregate, member) = __traits(getMember, aggregate, member));
+private enum isPublic(alias aggregate, string member) = !__traits(getProtection, __traits(getMember, aggregate, member)).privateOrPackage;
+
 private template isField(alias aggregate, string member)
 {
 	enum bool isField = __traits(compiles, __traits(getMember, aggregate, member).offsetof);
 }
 
+// check if the member is readable
+private enum bool isReadable(alias aggregate, string member) =
+	__traits(compiles, { static fun(T)(auto ref T t) {} fun(__traits(getMember, aggregate, member)); });
+
+// This trait defines what members should be processed -
+// public members that are either readable and writable or getter properties
+private template Processable(alias value, string member)
+{
+	static if (!isPublic!(value, member))
+		enum Processable = false;
+	else
+		enum Processable = isReadable!(value, member); // any readable is good
+}
+
 private template FieldsAndProperties(alias value)
 {
+	import std.meta : ApplyLeft, AliasSeq, Filter;
+
 	alias T = typeof(value);
 	alias isProperty = ApplyLeft!(.isProperty, value);
 	alias isField = ApplyLeft!(.isField, value);
@@ -35,14 +60,21 @@ private template getAllMembersImpl(T)
 		alias getAllMembersImpl = __traits(allMembers, T);
 }
 
+import std.meta : Reverse, NoDuplicates;
 private alias getAllMembers(T) = Reverse!(NoDuplicates!(Reverse!(getAllMembersImpl!T)));
 
 /// returns alias sequence, members of which are members of value
 /// that should be processed
-template ProcessableMembers(alias value)
+template ProcessableMembers(alias a)
 {
+	import std.traits : isType;
 	import std.meta : ApplyLeft, Filter;
+
+	static if (isType!a)
+		a value;
+	else
+		alias value = a;
 	alias AllMembers = FieldsAndProperties!value;
-	alias isProper = ApplyLeft!(Serializable, value);
-	alias SerializableMembers = Filter!(isProper, AllMembers);
+	alias isProper = ApplyLeft!(Processable, value);
+	alias ProcessableMembers = Filter!(isProper, AllMembers);
 }
